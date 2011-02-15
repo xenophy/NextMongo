799d5ac0051e3487c86c1f7d93aa3d496533aec3
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-02-15 18:18:00 +0900

    とりあえず、アプリケーションの原型作成

---
+++ public_html/src/app/App.js
@@ -0,0 +1,38 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next Mongo
+ * Copyright(c) 2006-2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.nextjs.net
+ */
+
+// {{{ Ext.app.App
+
+Application = Ext.app.App = new (Ext.extend(Object, {
+
+    // {{{ init
+
+    init : function() {
+
+
+
+    }
+
+    // }}}
+
+}));
+
+// }}}
+// {{{ onReady
+
+Ext.onReady(Application.init, Application);
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
